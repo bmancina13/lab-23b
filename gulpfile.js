e68c1bf28aed7fa0811cdd51e743cd82f6d45104
adesanto <aarondesanto@gmail.com>
2015-12-03 15:06:58 -0500

    node module del added and configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 var gulp = require('gulp'),
-	browserSync = require('browser-sync').create(),
-	stylus = require('gulp-stylus');
+		browserSync = require('browser-sync').create(),
+		stylus = require('gulp-stylus'),
+		concat = require('gulp-concat'),
+		del = require("del");
 
 gulp.task('stylus', function() {
-  gulp.src('demostyles.styl')
+  gulp.src('css/demostyles.styl')
     .pipe(stylus())
     .pipe(gulp.dest('css'))
     .pipe(browserSync.stream());
  });
- 
+
 gulp.task('serve', ['stylus'],function(){
 
 	browserSync.init({
 		server: "."
 	});
 
-	gulp.watch("demostyles.styl",['stylus']);
+	gulp.watch("css/demostyles.styl", ['stylus']);
 	gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+gulp.task('scripts', ["delete"], function() {
+  gulp.src('js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("delete", function() {
+	return del([
+		"js/all.js"
+	]);
 });
